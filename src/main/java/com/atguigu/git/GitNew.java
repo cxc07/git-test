4ed46a774524326e3ffb557ca914763ed6fc0bfa
ls07 <ls07@atguigu.com>
2024-07-30 23:45:17 +0800

    new first commit

---
+++ src/main/java/com/atguigu/git/GitNew.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author lishuai
+ * @date 2024/7/30-23:42
+ */
+public class GitNew {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
