672d2f14985ab6146b04ca7a5c643ef20c96165b
cxc07 <94284571+cxc07@users.noreply.github.com>
2024-07-31 23:39:07 +0800

    pull test commit

--- src/main/java/com/atguigu/git/GitNew.java
+++ src/main/java/com/atguigu/git/GitNew.java
@@ -10,5 +10,6 @@ public class GitNew {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("git pull test");
     }
 }
